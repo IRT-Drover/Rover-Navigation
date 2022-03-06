4ff0463932790c1d01bbf63f87a4c13d1b995808
Charles0208 <cjiang0208@icloud.com>
2022-03-06 13:56:50 -0500

    Navigation Testing Scripts

---
+++ FieldNavScript.py
@@ -0,0 +1,97 @@
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+from pymavlink import mavutil
+import time
+
+import argparse
+parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
+parser.add_argument('--connect',
+                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+connection_string = args.connect
+sitl = None
+
+
+# Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+
+# Connect to the Vehicle
+print('Connecting to vehicle on: %s' % connection_string)
+vehicle = connect(connection_string, wait_ready=True)
+
+# Function to arm
+def arm():
+
+  print("Basic pre-arm checks")
+  # Don't let the user try to arm until autopilot is ready
+  while not vehicle.is_armable:
+    print(" Waiting for vehicle to initialise...")
+    time.sleep(1)
+
+  print("Arming motors")
+  # Copter should arm in GUIDED mode
+  vehicle.mode    = VehicleMode("GUIDED")
+  vehicle.armed   = True
+
+  while not vehicle.armed:
+    print(" Waiting for arming...")
+    time.sleep(1)
+
+  # THIS SECTION NOT RELEVANT TO ROVER (takeoff and altitude check only make sense for copter)
+
+  # print("Taking off!")
+  # vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+  # Check that vehicle has reached takeoff altitude
+  # while True:
+  #   print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+  #   #Break and return from function just below target altitude.
+  #   if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+  #     print("Reached target altitude")
+  #     break
+  #   time.sleep(1)
+
+# Initialize the takeoff sequence to 2m
+arm()
+print("Arming complete")
+
+vehicle.groundspeed = 5
+
+print("go to waypoint:")
+
+wp3 = LocationGlobalRelative(40.66899398812567, -74.4762637032213, 0)   # <- the 3rd argument is the altitude in meters. (set to 0 for rover)
+vehicle.simple_goto(wp3)
+
+
+# Hover for 10 seconds
+time.sleep(5)
+
+wp3 = LocationGlobalRelative(40.66925671268826, -74.47546702829133, 0)   # <- the 3rd argument is the altitude in meters. (set to 0 for rover)
+vehicle.simple_goto(wp3)
+
+
+# Hover for 10 seconds
+time.sleep(5)
+
+wp3 = LocationGlobalRelative(40.6696321527646, -74.47568491273012, 0)   # <- the 3rd argument is the altitude in meters. (set to 0 for rover)
+vehicle.simple_goto(wp3)
+
+
+# Hover for 10 seconds
+time.sleep(5)
+
+wp3 = LocationGlobalRelative(40.66937112467311, -74.47648047031119, 0)   # <- the 3rd argument is the altitude in meters. (set to 0 for rover)
+vehicle.simple_goto(wp3)
+
+
+# Hover for 10 seconds
+time.sleep(5)
+
+print("return to starting point")
+vehicle.mode = VehicleMode("RTL")
+# Close vehicle object
+vehicle.close()
---
+++ ShortLineNavScript.py
@@ -0,0 +1,83 @@
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+from pymavlink import mavutil
+import time
+
+import argparse
+parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
+parser.add_argument('--connect',
+                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+connection_string = args.connect
+sitl = None
+
+
+# Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+
+# Connect to the Vehicle
+print('Connecting to vehicle on: %s' % connection_string)
+vehicle = connect(connection_string, wait_ready=True)
+
+# Function to arm
+def arm():
+
+  print("Basic pre-arm checks")
+  # Don't let the user try to arm until autopilot is ready
+  while not vehicle.is_armable:
+    print(" Waiting for vehicle to initialise...")
+    time.sleep(1)
+
+  print("Arming motors")
+  # Copter should arm in GUIDED mode
+  vehicle.mode    = VehicleMode("GUIDED")
+  vehicle.armed   = True
+
+  while not vehicle.armed:
+    print(" Waiting for arming...")
+    time.sleep(1)
+
+  # THIS SECTION NOT RELEVANT TO ROVER (takeoff and altitude check only make sense for copter)
+
+  # print("Taking off!")
+  # vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+  # Check that vehicle has reached takeoff altitude
+  # while True:
+  #   print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+  #   #Break and return from function just below target altitude.
+  #   if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+  #     print("Reached target altitude")
+  #     break
+  #   time.sleep(1)
+
+# Initialize the takeoff sequence to 2m
+arm()
+print("Arming complete")
+
+vehicle.groundspeed = 2
+
+print("go to waypoint:")
+
+wp3 = LocationGlobalRelative(40.668951465990915, -74.47647149057701, 0)   # <- the 3rd argument is the altitude in meters. (set to 0 for rover)
+vehicle.simple_goto(wp3)
+
+
+# Hover for 5 seconds
+time.sleep(5)
+
+wp3 = LocationGlobalRelative(40.669011044733914, -74.47629777550021, 0)   # <- the 3rd argument is the altitude in meters. (set to 0 for rover)
+vehicle.simple_goto(wp3)
+
+
+# Hover for 10 seconds
+time.sleep(10)
+
+print("return to starting point")
+vehicle.mode = VehicleMode("RTL")
+# Close vehicle object
+vehicle.close()
